d115be33dc4ab65905ce51a7322cce7de7e9141b
Mikhail Durmanov <madurmanov@gmail.com>
2018-02-07 18:03:03 +0300

    Update using for library

--- index.js
+++ index.js
@@ -2,35 +2,18 @@ const env = process.env.BABEL_ENV || process.env.NODE_ENV
 const dev = env !== 'production'
 
 module.exports = function getPreset(context, opts) {
-  if (opts && opts.library) {
-    const options = {
-      targets: {
-        browsers: [
-          'last 2 versions',
-          'safari >= 7',
-          'ie 11',
-        ],
-      },
-      loose: true,
-    }
-
-    return {
-      presets: [
-        [require.resolve('babel-preset-env'), options],
-      ],
-      plugins: [
-        require.resolve('babel-plugin-syntax-object-rest-spread'),
-        require.resolve('babel-plugin-syntax-export-extensions'),
-        require.resolve('babel-plugin-syntax-class-properties'),
-
-        [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
-        require.resolve('babel-plugin-transform-export-extensions'),
-        require.resolve('babel-plugin-transform-class-properties'),
-      ],
-    }
-  }
-
+  const library = opts && opts.library
   const targets = opts && opts.targets
+  const options = {
+    targets: {
+      browsers: [
+        'last 2 versions',
+        'safari >= 7',
+        'ie 11',
+      ],
+    },
+    loose: true,
+  }
   const node = {
     targets: { node: 'current' },
   }
@@ -42,26 +25,33 @@ module.exports = function getPreset(context, opts) {
   }
 
   return {
-    presets: [
+    presets: library ? [
+      [require.resolve('babel-preset-env'), options],
+    ] : [
       [require.resolve('babel-preset-env'), targets ? browser : node],
       require.resolve('babel-preset-react'),
     ],
     plugins: [
-      require.resolve('babel-plugin-universal-import'),
-      targets && require.resolve('babel-plugin-syntax-dynamic-import'),
-      !targets && require.resolve('babel-plugin-dynamic-import-node'),
-
       require.resolve('babel-plugin-syntax-object-rest-spread'),
       require.resolve('babel-plugin-syntax-export-extensions'),
       require.resolve('babel-plugin-syntax-class-properties'),
-
-      [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+      [
+        require.resolve('babel-plugin-transform-object-rest-spread'),
+        { useBuiltIns: true },
+      ],
       require.resolve('babel-plugin-transform-export-extensions'),
       require.resolve('babel-plugin-transform-class-properties'),
-
-      [require.resolve('babel-plugin-transform-react-jsx'), { useBuiltIns: true }],
-      dev && require.resolve('babel-plugin-transform-react-jsx-source'),
-      dev && require.resolve('babel-plugin-transform-react-jsx-self'),
-    ].filter(Boolean),
+    ]
+      .concat(!library ? [
+        require.resolve('babel-plugin-universal-import'),
+        targets && require.resolve('babel-plugin-syntax-dynamic-import'),
+        !targets && require.resolve('babel-plugin-dynamic-import-node'),
+        [
+          require.resolve('babel-plugin-transform-react-jsx'),
+          { useBuiltIns: true },
+        ],
+        dev && require.resolve('babel-plugin-transform-react-jsx-source'),
+        dev && require.resolve('babel-plugin-transform-react-jsx-self'),
+      ] : []).filter(Boolean),
   }
 }
