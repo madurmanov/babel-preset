efafc07de8ed7f9646c7e8622c94a02fb525c250
Mikhail Durmanov <madurmanov@gmail.com>
2018-02-08 12:20:06 +0300

    Update configuration

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const env = process.env.BABEL_ENV || process.env.NODE_ENV
-const dev = env !== 'production'
+const DEV = process.env.NODE_ENV !== 'production'
 
 module.exports = function getPreset(context, opts) {
-  const library = opts && opts.library
   const targets = opts && opts.targets
   const alias = {
     src: './src',
-  }
-  const options = {
-    targets: {
-      browsers: [
-        'last 2 versions',
-        'safari >= 7',
-        'ie 11',
-      ],
-    },
-    loose: true,
   }
   const node = {
     targets: { node: 'current' },
@@ -28,34 +16,28 @@ module.exports = function getPreset(context, opts) {
   }
 
   return {
-    presets: library ? [
-      [require.resolve('babel-preset-env'), options],
-    ] : [
+    presets: [
       [require.resolve('babel-preset-env'), targets ? browser : node],
       require.resolve('babel-preset-react'),
+      require.resolve('babel-preset-stage-2'),
     ],
     plugins: [
-      require.resolve('babel-plugin-syntax-object-rest-spread'),
-      require.resolve('babel-plugin-syntax-export-extensions'),
-      require.resolve('babel-plugin-syntax-class-properties'),
+      [require.resolve('babel-plugin-module-resolver'), { alias }],
+      require.resolve('babel-plugin-universal-import'),
+      !targets && require.resolve('babel-plugin-dynamic-import-node'),
       [
-        require.resolve('babel-plugin-transform-object-rest-spread'),
+        require.resolve('babel-plugin-transform-react-jsx'),
         { useBuiltIns: true },
       ],
-      require.resolve('babel-plugin-transform-export-extensions'),
-      require.resolve('babel-plugin-transform-class-properties'),
-    ]
-      .concat(!library ? [
-        [require.resolve('babel-plugin-module-resolver'), { alias }],
-        require.resolve('babel-plugin-universal-import'),
-        targets && require.resolve('babel-plugin-syntax-dynamic-import'),
-        !targets && require.resolve('babel-plugin-dynamic-import-node'),
-        [
-          require.resolve('babel-plugin-transform-react-jsx'),
-          { useBuiltIns: true },
+      DEV && require.resolve('babel-plugin-transform-react-jsx-source'),
+      DEV && require.resolve('babel-plugin-transform-react-jsx-self'),
+    ].filter(Boolean),
+    env: {
+      development: {
+        plugins: [
+          'react-hot-loader/babel',
         ],
-        dev && require.resolve('babel-plugin-transform-react-jsx-source'),
-        dev && require.resolve('babel-plugin-transform-react-jsx-self'),
-      ] : []).filter(Boolean),
+      },
+    },
   }
 }
