22475f4d3cbbdeb4a7749e0fc1dd037cacaf905f
Mikhail Durmanov <madurmanov@gmail.com>
2018-02-09 19:35:58 +0300

    Update configuration

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-const DEV = process.env.NODE_ENV !== 'production'
-
-module.exports = function getPreset(context, opts) {
-  const targets = opts && opts.targets
-  const alias = {
-    src: './src',
-  }
-  const node = {
-    targets: { node: 'current' },
-  }
-  const browser = {
-    targets,
-    useBuiltIns: false,
-    modules: false,
-    loose: true,
-  }
-
+module.exports = function getPreset() {
   return {
     presets: [
-      [require.resolve('babel-preset-env'), targets ? browser : node],
-      require.resolve('babel-preset-react'),
-      require.resolve('babel-preset-stage-2'),
-    ],
+      'babel-preset-env',
+      'babel-preset-react',
+      'babel-preset-stage-2',
+    ].map(require.resolve),
     plugins: [
-      [require.resolve('babel-plugin-module-resolver'), { alias }],
-      require.resolve('babel-plugin-universal-import'),
-      !targets && require.resolve('babel-plugin-dynamic-import-node'),
       [
-        require.resolve('babel-plugin-transform-react-jsx'),
-        { useBuiltIns: true },
+        require.resolve('babel-plugin-universal-import'),
+        {
+          disableWarnings: true,
+        },
       ],
-      DEV && require.resolve('babel-plugin-transform-react-jsx-source'),
-      DEV && require.resolve('babel-plugin-transform-react-jsx-self'),
-    ].filter(Boolean),
+    ],
     env: {
       development: {
         plugins: [
           'react-hot-loader/babel',
-        ],
+        ].map(require.resolve),
       },
     },
   }
